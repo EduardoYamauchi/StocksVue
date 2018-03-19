b057f44fdd568d31e30cef8108351f0800ad4e53
Eduardo Yamauchi <yamauchieduardo@gmail.com>
2018-03-19 00:17:25 -0400

    Router fix

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
-  </div>
+    <div id="app" class="container">
+      <router-view></router-view>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+
+    }
 </script>
+export default {
+  name: 'App'
+}
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import VueRouter from 'vue-router'
+import App from './App.vue'
+import { routes } from './routes'
 
-Vue.config.productionTip = false
+Vue.use(VueRouter)
 
-/* eslint-disable no-new */
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
 new Vue({
   el: '#app',
-  components: { App },
-  template: '<App/>'
+  router,
+  render: h => h(App)
 })
